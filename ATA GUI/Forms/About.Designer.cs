fb5e256cbb96deb2a1d8354e41ad2b8d33d6576c
Massimiliano Sartore <56435388+MassimilianoSartore@users.noreply.github.com>
2021-03-23 12:26:24 +0100

    About version update to v1.2.0

--- ATA GUI/Forms/About.Designer.cs
+++ ATA GUI/Forms/About.Designer.cs
@@ -31,16 +31,16 @@ namespace ATA_GUI
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(About));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.buttonClose = new System.Windows.Forms.Button();
-            this.linkLabelDNZ = new System.Windows.Forms.LinkLabel();
-            this.linkLabelSDK = new System.Windows.Forms.LinkLabel();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.linkLabelRepo = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.linkLabelSDK = new System.Windows.Forms.LinkLabel();
+            this.linkLabelDNZ = new System.Windows.Forms.LinkLabel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.linkLabelRepo = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
@@ -57,6 +57,25 @@ namespace ATA_GUI
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Program Version";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Version: 1.2.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ATA GUI (Advance Tool for Android™)";
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.linkLabelSDK);
@@ -67,6 +86,28 @@ namespace ATA_GUI
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Assest Used";
+            // 
+            // linkLabelSDK
+            // 
+            this.linkLabelSDK.AutoSize = true;
+            this.linkLabelSDK.Location = new System.Drawing.Point(12, 35);
+            this.linkLabelSDK.Name = "linkLabelSDK";
+            this.linkLabelSDK.Size = new System.Drawing.Size(94, 13);
+            this.linkLabelSDK.TabIndex = 1;
+            this.linkLabelSDK.TabStop = true;
+            this.linkLabelSDK.Text = "SDK Platform Tool";
+            this.linkLabelSDK.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSDK_LinkClicked);
+            // 
+            // linkLabelDNZ
+            // 
+            this.linkLabelDNZ.AutoSize = true;
+            this.linkLabelDNZ.Location = new System.Drawing.Point(12, 16);
+            this.linkLabelDNZ.Name = "linkLabelDNZ";
+            this.linkLabelDNZ.Size = new System.Drawing.Size(56, 13);
+            this.linkLabelDNZ.TabIndex = 0;
+            this.linkLabelDNZ.TabStop = true;
+            this.linkLabelDNZ.Text = "DotNetZip";
+            this.linkLabelDNZ.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelDNZ_LinkClicked);
             // 
             // groupBox3
             // 
@@ -80,65 +121,14 @@ namespace ATA_GUI
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Developer Info";
             // 
-            // buttonClose
+            // label4
             // 
-            this.buttonClose.Location = new System.Drawing.Point(298, 192);
-            this.buttonClose.Name = "buttonClose";
-            this.buttonClose.Size = new System.Drawing.Size(75, 23);
-            this.buttonClose.TabIndex = 3;
-            this.buttonClose.Text = "Close";
-            this.buttonClose.UseVisualStyleBackColor = true;
-            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
-            // 
-            // linkLabelDNZ
-            // 
-            this.linkLabelDNZ.AutoSize = true;
-            this.linkLabelDNZ.Location = new System.Drawing.Point(12, 16);
-            this.linkLabelDNZ.Name = "linkLabelDNZ";
-            this.linkLabelDNZ.Size = new System.Drawing.Size(56, 13);
-            this.linkLabelDNZ.TabIndex = 0;
-            this.linkLabelDNZ.TabStop = true;
-            this.linkLabelDNZ.Text = "DotNetZip";
-            this.linkLabelDNZ.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelDNZ_LinkClicked);
-            // 
-            // linkLabelSDK
-            // 
-            this.linkLabelSDK.AutoSize = true;
-            this.linkLabelSDK.Location = new System.Drawing.Point(12, 35);
-            this.linkLabelSDK.Name = "linkLabelSDK";
-            this.linkLabelSDK.Size = new System.Drawing.Size(94, 13);
-            this.linkLabelSDK.TabIndex = 1;
-            this.linkLabelSDK.TabStop = true;
-            this.linkLabelSDK.Text = "SDK Platform Tool";
-            this.linkLabelSDK.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelSDK_LinkClicked);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 14);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "ATA GUI (Advance Tool for Android™)";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 32);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(81, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Version: 1.0.0.1";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 16);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(103, 13);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Massimiliano Sartore";
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(265, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Copyright © 2021";
             // 
             // linkLabelRepo
             // 
@@ -151,14 +141,24 @@ namespace ATA_GUI
             this.linkLabelRepo.Text = "Repository";
             this.linkLabelRepo.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelRepo_LinkClicked);
             // 
-            // label4
+            // label3
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(265, 16);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 13);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Copyright © 2021";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Massimiliano Sartore";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(298, 192);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
             // 
             // About
             // 
