db5f96c9abfa03fd7c17b05c897f1f5425175f69
Massimiliano Sartore <massimiliano.sartore@protonmail.com>
2023-10-01 21:48:02 +0200

    bump version to v2.7.1

--- ATA-GUI/Classes/ATA.cs
+++ ATA-GUI/Classes/ATA.cs
@@ -9,7 +9,7 @@ namespace ATA_GUI.Classes
 {
     internal class ATA
     {
-        public static readonly string CURRENTVERSION = "v2.7.0";
+        public static readonly string CURRENTVERSION = "v2.7.1";
         public static readonly string IPFileName = "IPList.txt";
 
         public ATA()
