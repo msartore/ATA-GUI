eded0a9bb14da001cea5013b6994c88e6ad23718
Massimiliano Sartore <massimiliano.sartore@protonmail.com>
2024-07-22 15:42:30 +0200

    delete website

--- docs/script.js
+++
@@ -1,7 +0,0 @@
-function openAndCloseNav() {
-    var sidenav = document.getElementById("sidenav")
-    var navButtonClose = document.getElementById("navButtonClose")
-    var visibility = sidenav.style.visibility == "visible" ? 'collapse' : "visible"
-    sidenav.style.visibility = visibility
-    navButtonClose.style.visibility = visibility
-}
